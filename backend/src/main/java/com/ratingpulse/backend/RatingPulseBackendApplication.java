6ad8b58f075690a8674a1187908fd3970d2e554e
danielecitran <daniele.ctr.zh@icloud.com>
2025-04-02 10:18:27 +0200

    Set up initial Spring Boot project (backend)

---
+++ backend/src/main/java/com/ratingpulse/backend/RatingPulseBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ratingpulse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingPulseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingPulseBackendApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/ratingpulse/backend/RatingPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratingpulse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingPulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
